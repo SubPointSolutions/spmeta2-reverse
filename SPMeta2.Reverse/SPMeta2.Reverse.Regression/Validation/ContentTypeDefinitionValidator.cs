8c0ddefada841343880befd35a262da60a8a59d9
Support <support@subpointsolutions.com>
2016-04-04 08:02:25 +1000

    + addec content type tests and validation

---
+++ SPMeta2.Reverse/SPMeta2.Reverse.Regression/Validation/ContentTypeDefinitionValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPMeta2.Definitions;
+using SPMeta2.Reverse.Regression.Base;
+using SPMeta2.Syntax.Default;
+using SPMeta2.Utils;
+
+namespace SPMeta2.Reverse.Regression.Validation
+{
+    public class ContentTypeDefinitionValidator : TypedReverseDefinitionValidatorBase<ContentTypeDefinition>
+    {
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var typedModelHost = modelHost.WithAssertAndCast<ReverseValidationModeHost>("modelHost", m => m.RequireNotNull());
+
+            var originalDefinition = typedModelHost.OriginalModel.Value.WithAssertAndCast<ContentTypeDefinition>("value", m => m.RequireNotNull());
+            var reversedDefinition = typedModelHost.ReversedModel.Value.WithAssertAndCast<ContentTypeDefinition>("value", m => m.RequireNotNull());
+
+            var assert = ServiceFactory.AssertService.NewAssert(originalDefinition, reversedDefinition);
+
+            assert
+                .ShouldBeEqual(s => s.Name, r => r.Name)
+                .ShouldBeEqual(s => s.Description, r => r.Description)
+                .ShouldBeEqual(s => s.Group, r => r.Group)
+                .ShouldBeEqual(s => s.GetContentTypeId(), r => r.GetContentTypeId());
+        }
+    }
+}
---
+++ SPMeta2.Reverse/SPMeta2.Reverse.Tests/Impl/Definitions/ContentTypeDefinitionTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPMeta2.Containers;
+using SPMeta2.Definitions;
+using SPMeta2.Reverse.CSOM.Foundation.ReverseHandlers;
+using SPMeta2.Reverse.CSOM.Foundation.Services;
+using SPMeta2.Reverse.Services;
+using SPMeta2.Reverse.Tests.Base;
+using SPMeta2.Syntax.Default;
+
+namespace SPMeta2.Reverse.Tests.Impl.Definitions
+{
+    [TestClass]
+    public class ContentTypeDefinitionTests : ReverseTestBase
+    {
+        #region tests
+
+        [TestMethod]
+        [TestCategory("ContentTypes")]
+        public void Can_Reverse_Site_ContentTypes()
+        {
+            var model = SPMeta2Model.NewSiteModel(site =>
+            {
+                site.AddRandomContentType();
+                site.AddRandomContentType();
+            });
+
+            DeployReverseAndTestModel(model);
+        }
+
+        [TestMethod]
+        [TestCategory("ContentTypes")]
+        public void Can_Reverse_Web_ContentTypes()
+        {
+            var model = SPMeta2Model.NewWebModel(web =>
+            {
+                web.AddRandomContentType();
+                web.AddRandomContentType();
+            });
+
+            DeployReverseAndTestModel(model);
+        }
+
+        #endregion
+    }
+}
