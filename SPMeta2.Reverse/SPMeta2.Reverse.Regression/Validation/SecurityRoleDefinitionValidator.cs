731c32604687d0d16388f6b68535f28794e67bdb
Support <support@subpointsolutions.com>
2016-04-04 19:45:58 +1000

    + Add SecurityRoleDefinition reverse handler #22

---
+++ SPMeta2.Reverse/SPMeta2.Reverse.Regression/Validation/SecurityRoleDefinitionValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPMeta2.Definitions;
+using SPMeta2.Reverse.Regression.Base;
+using SPMeta2.Utils;
+
+namespace SPMeta2.Reverse.Regression.Validation
+{
+    public class SecurityRoleDefinitionValidator : TypedReverseDefinitionValidatorBase<SecurityRoleDefinition>
+    {
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            var typedModelHost = modelHost.WithAssertAndCast<ReverseValidationModeHost>("modelHost", m => m.RequireNotNull());
+
+            var originalDefinition = typedModelHost.OriginalModel.Value.WithAssertAndCast<SecurityRoleDefinition>("value", m => m.RequireNotNull());
+            var reversedDefinition = typedModelHost.ReversedModel.Value.WithAssertAndCast<SecurityRoleDefinition>("value", m => m.RequireNotNull());
+
+            var assert = ServiceFactory.AssertService.NewAssert(originalDefinition, reversedDefinition);
+
+            assert
+                .ShouldBeEqual(s => s.Name, r => r.Name)
+                .ShouldBeEqual(s => s.Description, r => r.Description)
+                ;
+        }
+    }
+}
