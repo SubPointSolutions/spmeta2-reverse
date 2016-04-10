2b3967d5b57ca039c46b95d30530db028ad6204f
Support <support@subpointsolutions.com>
2016-04-11 08:37:55 +1000

    Add NumberFieldDefinition reverse handler #42

---
+++ SPMeta2.Reverse/SPMeta2.Reverse.Regression/Validation/Fields/NumberFieldDefinitionValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SPMeta2.Definitions;
+using SPMeta2.Definitions.Fields;
+using SPMeta2.Reverse.Regression.Base;
+using SPMeta2.Utils;
+
+namespace SPMeta2.Reverse.Regression.Validation.Fields
+{
+    public class NumberFieldDefinitionValidator : FieldDefinitionValidator
+    {
+        public override Type TargetType
+        {
+            get { return typeof(NumberFieldDefinition); }
+        }
+
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            base.DeployModel(modelHost, model);
+
+            var typedModelHost = modelHost.WithAssertAndCast<ReverseValidationModeHost>("modelHost", m => m.RequireNotNull());
+
+            var originalDefinition = typedModelHost.OriginalModel.Value.WithAssertAndCast<NumberFieldDefinition>("value", m => m.RequireNotNull());
+            var reversedDefinition = typedModelHost.ReversedModel.Value.WithAssertAndCast<NumberFieldDefinition>("value", m => m.RequireNotNull());
+
+            var assert = ServiceFactory.AssertService.NewAssert(originalDefinition, reversedDefinition);
+
+            assert
+                .ShouldBeEqual(s => s.DisplayFormat, r => r.DisplayFormat)
+                .ShouldBeEqual(s => s.MinimumValue, r => r.MinimumValue)
+                .ShouldBeEqual(s => s.MaximumValue, r => r.MaximumValue);
+        }
+    }
+}
