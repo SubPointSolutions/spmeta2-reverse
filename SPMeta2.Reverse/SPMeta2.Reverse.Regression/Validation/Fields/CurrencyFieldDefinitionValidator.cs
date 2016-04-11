81bb023405797234d2b2a1df190d0ae355b83fd6
Support <support@subpointsolutions.com>
2016-04-11 18:03:03 +1000

    + Add CurrencyFieldDefinition reverse handler #34

---
+++ SPMeta2.Reverse/SPMeta2.Reverse.Regression/Validation/Fields/CurrencyFieldDefinitionValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SPMeta2.Definitions;
+using SPMeta2.Definitions.Fields;
+using SPMeta2.Reverse.Regression.Base;
+using SPMeta2.Utils;
+using SPMeta2.Containers.Assertion;
+
+namespace SPMeta2.Reverse.Regression.Validation.Fields
+{
+    public class CurrencyFieldDefinitionValidator : FieldDefinitionValidator
+    {
+        public override Type TargetType
+        {
+            get { return typeof(CurrencyFieldDefinition); }
+        }
+
+        public override void DeployModel(object modelHost, DefinitionBase model)
+        {
+            base.DeployModel(modelHost, model);
+
+            var typedModelHost = modelHost.WithAssertAndCast<ReverseValidationModeHost>("modelHost", m => m.RequireNotNull());
+
+            var originalDefinition = typedModelHost.OriginalModel.Value.WithAssertAndCast<CurrencyFieldDefinition>("value", m => m.RequireNotNull());
+            var reversedDefinition = typedModelHost.ReversedModel.Value.WithAssertAndCast<CurrencyFieldDefinition>("value", m => m.RequireNotNull());
+
+            var assert = ServiceFactory.AssertService.NewAssert(originalDefinition, reversedDefinition);
+
+            // TODO, typed field validation
+            //assert
+            //    .ShouldBeEqual(s => s.EditFormat, r => r.EditFormat)
+            //    .ShouldBeEqual(s => s.FillInChoice, r => r.FillInChoice);
+        }
+    }
+}
---
+++ SPMeta2.Reverse/SPMeta2.Reverse.Tests/Impl/Definitions/Fields/CurrencyFieldDefinitionTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPMeta2.Containers;
+using SPMeta2.Definitions;
+using SPMeta2.Definitions.Fields;
+using SPMeta2.Reverse.Services;
+using SPMeta2.Reverse.Tests.Base;
+using SPMeta2.Syntax.Default;
+
+namespace SPMeta2.Reverse.Tests.Impl.Definitions.Fields
+{
+    [TestClass]
+    public class CurrencyFieldDefinitionTests : ReverseTestBase
+    {
+        #region tests
+
+        [TestMethod]
+        [TestCategory("Fields.Currency")]
+        public void Can_Reverse_Site_CurrencyFields()
+        {
+            var model = SPMeta2Model.NewSiteModel(site =>
+            {
+                site.AddCurrencyField(Def<CurrencyFieldDefinition>());
+                site.AddCurrencyField(Def<CurrencyFieldDefinition>());
+            });
+
+            DeployReverseAndTestModel(model);
+        }
+
+        [TestMethod]
+        [TestCategory("Fields.Currency")]
+        public void Can_Reverse_Web_CurrencyFields()
+        {
+            var options = ReverseOptions.Default
+                            .AddDepthOption<WebDefinition>(0);
+
+            var model = SPMeta2Model.NewWebModel(web =>
+            {
+                web.AddCurrencyField(Def<CurrencyFieldDefinition>());
+                web.AddCurrencyField(Def<CurrencyFieldDefinition>());
+            });
+
+            DeployReverseAndTestModel(model,options);
+        }
+
+        #endregion
+    }
+}
